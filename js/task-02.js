44914cff4703e6c7c659a8167e4dc91a1d6adf91
Eugene asaulyak <jenya.asaulyak@gmail.com>
2023-11-11 20:16:06 +0200

    added task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,21 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+function createElements() {
+  const listItemElements = ingredients.map(ingredient => {
+    const listItem = document.createElement('li');
+    listItem.textContent = ingredient;
+    listItem.classList.add('item');
+
+    return listItem;
+  });
+
+  const listContainerElement = document.getElementById('ingredients');
+
+  if (!listContainerElement) {
+    return;
+  }
+
+  listContainerElement.append(...listItemElements);
+}
+
+createElements();
